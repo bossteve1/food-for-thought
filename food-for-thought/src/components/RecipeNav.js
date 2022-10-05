172db4f70181fd448de2bc2c08d020098a59bc56
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-05 16:24:40 +0300

    call api

--- food-for-thought/src/components/RecipeNav.js
+++ food-for-thought/src/components/RecipeNav.js
@@ -5,15 +5,15 @@ import { NavLink } from 'react-router-dom'
 function RecipeNav() {
   return (
     
-    <Nav className="bg-success text-white " defaultActiveKey="/" as="ul">
+    <Nav className="bg-success text-white "as="ul">
     <p className='text-black fw-bold navbar-brand pt-2 me-auto fs-3 fst-italic'>
       Food4Thought
     </p>
-    <NavLink className='m-3 text-white fw-bold' to="/" exact > Home</NavLink>
-    <NavLink className='m-3 text-white fw-bold' to="/recipes" exact >Recipes</NavLink>
-    <NavLink className='m-3 text-white fw-bold' to="/indian" exact > Indian</NavLink>
-    <NavLink className='m-3 text-white fw-bold' to="/chinese" ecact > Chinese</NavLink>
-    <NavLink className='m-3 text-white fw-bold' to="/american" exact >American</NavLink>
+    <NavLink className='m-3 text-white fw-bold' to="/"  > Home</NavLink>
+    <NavLink className='m-3 text-white fw-bold' to="/recipes"  >Recipes</NavLink>
+    <NavLink className='m-3 text-white fw-bold' to="/indian"  > Indian</NavLink>
+    <NavLink className='m-3 text-white fw-bold' to="/chinese"  > Chinese</NavLink>
+    <NavLink className='m-3 text-white fw-bold' to="/american"  >American</NavLink>
     
   </Nav>
   )
--- food-for-thought/src/pages/Indian.js
+++ food-for-thought/src/pages/Indian.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RecipeCard from '../components/RecipeCard';
 import RecipeNav from '../components/RecipeNav';
 
 
 function Indian() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=Indian")
+      .then((r) => r.json())
+      .then((recipes) => setRecipes(recipes.meals));
+  }, []);
+
   return (
     <>
     <RecipeNav/>
-    <RecipeCard/>
-
+    {recipes.map((recipe) => (
+      <RecipeCard key={recipe.id}/>
+    ))}
     
     </>
   )
