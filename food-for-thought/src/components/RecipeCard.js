59fc075f29a0470c2b85b59b74c7898da5be3716
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-05 15:31:41 +0300

    add and style modal

--- food-for-thought/src/components/RecipeCard.js
+++ food-for-thought/src/components/RecipeCard.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { Button, Card } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Card, Modal } from 'react-bootstrap'
 
 function RecipeCard() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   return (
-<Card style={{ width: '18rem' }}>
+    <>
+    <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
@@ -11,9 +18,19 @@ function RecipeCard() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="success">Get Recipe</Button>
+        <Button  onClick={handleShow} variant="success">Get Recipe</Button>
       </Card.Body>
-    </Card>  )
+    </Card>  
+
+    <Modal show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Modal heading</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+ 
+  </Modal>
+  </>
+  )
 }
 
 export default RecipeCard
