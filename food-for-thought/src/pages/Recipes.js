8277ac51c532b4fe64f7c7543a1c22defcbcbfb4
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-05 17:29:36 +0300

    add details to recipe card

--- food-for-thought/src/pages/Recipes.js
+++ food-for-thought/src/pages/Recipes.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import RecipeCard from '../components/RecipeCard';
 import RecipeNav from '../components/RecipeNav';
 
 function Recipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((r) => r.json())
+      .then((recipes) => setRecipes(recipes.meals));
+  }, []);
+
   return (
     <>
     <RecipeNav/>
-    <RecipeCard/>
+    <Container>
+      <Row>
+          {recipes.map((recipe) => (
+            <Col xs={12} md={4}className='p-5'>
+
+            <RecipeCard recipe={recipe} key={recipe.id}/>
+            </Col>
+          ))}
+       
+      </Row>
+    </Container>
+
     
     </>
   )
